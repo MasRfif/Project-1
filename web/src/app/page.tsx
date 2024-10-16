f6832bf865dc9e25abee69ba22fb64a7354c9c3c
MasRfif <odri.naufal@gmail.com>
2024-10-16 16:56:58 +0700

    tweeking on WEB

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,8 +2,12 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-2xl font-semibold p-10 text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
-        Hello
+        Wellcome To Gallery Input Page
       </h1>
+      click here to start Producing
+      <Button>
+        <Link href="/home">Painting</Link>
+      </Button>
     </div>
   );
 };
