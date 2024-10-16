83c040687202adb89cf6a45a175881a60d76e911
MasRfif <odri.naufal@gmail.com>
2024-10-16 17:26:36 +0700

    tweeking

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,12 +2,20 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-semibold p-10 text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
-        Wellcome To Gallery Input Page
-      </h1>
-      click here to start Producing
-      <Link href="/home">Painting</Link>
+    <div
+      className="w-full bg-center bg-cover h-[38rem]"
+      style="background-image: url('https://images.unsplash.com/photo-1556761175-b413da4baf72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');">
+      <div className="flex items-center justify-center w-full h-full bg-gray-900/40">
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold text-white lg:text-4xl">
+            Build your new <span className="text-[#c9802c]">Gallery</span> Now!
+          </h1>
+          <button className="w-full px-5 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-[#a5743c] rounded-md lg:w-auto hover:bg-[#533c21] focus:outline-none focus:bg-[#a5743c]">
+            <link href="/home" />
+            <p>Let's start it</p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
